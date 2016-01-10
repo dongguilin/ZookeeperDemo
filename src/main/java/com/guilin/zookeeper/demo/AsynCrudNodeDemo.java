4a7bd5e008d5bc49d3d563947abe96f39d8c681b
dongguilin <guilin1_happy@163.com>
2016-01-10 14:04:39 +0800

    调整包结构

--- src/main/java/com/guilin/zookeeper/demo/AsynCrudNodeDemo.java
+++ src/main/java/com/guilin/zookeeper/demo/AsynCrudNodeDemo.java
@@ -1,4 +1,4 @@
-package com.guilin.zookeeper;
+package com.guilin.zookeeper.demo;
 
 /**
  * Created by hadoop on 2016/1/10.
--- src/main/java/com/guilin/zookeeper/demo/CrudNodeDemo.java
+++ src/main/java/com/guilin/zookeeper/demo/CrudNodeDemo.java
@@ -1,4 +1,4 @@
-package com.guilin.zookeeper;
+package com.guilin.zookeeper.demo;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
