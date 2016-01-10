7f07ab8123e59042a3b730861717c675a973dc36
dongguilin <guilin1_happy@163.com>
2016-01-10 22:19:11 +0800

    分布式锁InterProcessLock

---
+++ src/main/java/com/guilin/zookeeper/demo/RecipesLock.java
@@ -0,0 +1,56 @@
+package com.guilin.zookeeper.demo;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by hadoop on 2016/1/10.
+ * 使用Curator实现分布式锁功能
+ * InterProcessLock
+ */
+public class RecipesLock {
+    private static String lockPath = "/curator_recipes_lock_path";
+
+    private static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("aleiyeb:12181")
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+            .build();
+
+    public static void main(String[] args) {
+        client.start();
+
+        final InterProcessMutex lock = new InterProcessMutex(client, lockPath);
+        final CountDownLatch down = new CountDownLatch(1);
+        for (int i = 0; i < 30; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        down.await();
+                        lock.acquire();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
+                    String orderNo = sdf.format(new Date());
+                    System.out.println("生成订单号是：" + orderNo);
+                    try {
+                        lock.release();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+        down.countDown();
+    }
+
+}
---
+++ src/main/java/com/guilin/zookeeper/demo/RecipesNoLockDemo.java
@@ -0,0 +1,33 @@
+package com.guilin.zookeeper.demo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by hadoop on 2016/1/10.
+ * 一个典型时间戳生成的并发问题
+ */
+public class RecipesNoLockDemo {
+
+    public static void main(String[] args) {
+        final CountDownLatch down = new CountDownLatch(1);
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        down.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
+                    String orderNo = sdf.format(new Date());
+                    System.out.println("生成的订单号是：" + orderNo);
+                }
+            }).start();
+        }
+        down.countDown();
+    }
+
+}
