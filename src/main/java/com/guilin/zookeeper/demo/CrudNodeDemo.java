b17d416e3ea71fe3e4d3797598fbc97b06d7e468
dongguilin <guilin1_happy@163.com>
2016-03-06 17:15:07 +0800

    crud测试修改

--- src/main/java/com/guilin/zookeeper/demo/CrudNodeDemo.java
+++ src/main/java/com/guilin/zookeeper/demo/CrudNodeDemo.java
@@ -15,7 +15,7 @@ import org.junit.Test;
 public class CrudNodeDemo {
 
     private static CuratorFramework client;
-    private static String connectString = "aleiyeb:12181";
+    private static String connectString = "localhost:2181,localhost:2182,localhost:2183";
     private String path = "/zk-book/c1";
 
     @BeforeClass
@@ -45,7 +45,7 @@ public class CrudNodeDemo {
 
 //        client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(path, "init".getBytes());
         client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(path, "init".getBytes());
-        client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(path, "init".getBytes());
+//        client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(path, "init".getBytes());
 
 //        Thread.sleep(60*1000);
 
@@ -64,7 +64,7 @@ public class CrudNodeDemo {
     }
 
     private void create() throws Exception {
-        //创建一个节点，初始内容为空
+        //创建一个节点，初始内容为客户端IP地址
         client.create().forPath(path);
 
         //创建一个节点，附带初始内容
@@ -77,32 +77,54 @@ public class CrudNodeDemo {
         client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(path);
     }
 
-    private void get() throws Exception {
+    @Test
+    public void createNode() throws Exception {
+        client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath("/hello/a1/a2", "".getBytes());
+        client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath("/hello/a1/a2", "".getBytes());
+
+        client.create().withMode(CreateMode.EPHEMERAL).forPath("/e", "".getBytes());
+
+        client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/p/a", "".getBytes());
+        client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT_SEQUENTIAL).forPath("/p/b", "".getBytes());
+        client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT_SEQUENTIAL).forPath("/p/b", "".getBytes());
+        Thread.currentThread().join();
+    }
+
+    @Test
+    public void get() throws Exception {
         //读取一个节点的数据内容
-        client.getData().forPath(path);
+        String data = new String(client.getData().forPath(path));
+        System.out.println(data);
 
         //读取一个节点的数据内容，同时获取该节点的stat
         Stat stat = new Stat();
-        client.getData().storingStatIn(stat).forPath(path);
+        data = new String(client.getData().storingStatIn(stat).forPath(path));
+        System.out.println(data);
+        System.out.println(stat);
+
+        data = new String(client.getData().forPath("/p/a"));
+        System.out.println(null == data);
     }
 
-    private void update() throws Exception {
+    @Test
+    public void update() throws Exception {
         //更新一个节点的数据内容
         Stat stat = client.setData().forPath(path);
 
         //更新一个节点的数据内容，强制指定版本进行更新
-        client.setData().withVersion(stat.getVersion()).forPath(path);
+        client.setData().withVersion(stat.getVersion()).forPath(path, "hello,world".getBytes());
     }
 
-    private void delete() throws Exception {
+    @Test
+    public void delete() throws Exception {
         //删除一个节点，只能删除叶子节点
-        client.delete().forPath(path);
+//        client.delete().forPath(path);
 
         //删除一个节点，并递归删除其所有子节点
-        client.delete().deletingChildrenIfNeeded().forPath(path);
+//        client.delete().deletingChildrenIfNeeded().forPath(path);
 
         //删除一个节点，强制指定版本进行删除
-        client.delete().withVersion(2).forPath(path);
+//        client.delete().withVersion(6).forPath(path);
 
         //删除一个节点，强制保证删除（只要客户端会话有效，那么Curator会在后台持续进行删除操作，直到节点删除成功）
         client.delete().guaranteed().forPath(path);
